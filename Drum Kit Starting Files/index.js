b7e2e42b1871fb64290adbb74ff31c88b2caab6d
gushteraa <petkozlatilov1234@gmail.com>
2023-11-02 22:01:29 +0200

    +

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,19 +1,45 @@
-for (let i = 0; i < 7; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click", clickOnBtn)
+for (let i = 0; i < 7; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHtml = this.innerHTML;
+
+    makeSound(buttonInnerHtml);
+  });
+
+  document.addEventListener("keydown", function (event) {
+    makeSound(event.key);
+  });
 }
-
-function clickOnBtn(){
-
-    var buttonInnerHtml = this.innerHTML; 
-
-    switch (buttonInnerHtml) {
-        case 'w':
-            var audio = new Audio('sounds/crash.png')
-            audio.play()
-            break;
-    
-        default:
-            break;
-    }
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("./sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("./sounds/snare.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("./sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("./sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("./sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("./sounds/tom-4.mp3");
+      audio.play();
+      break;
+    default:
+      break;
+  }
 }
-
